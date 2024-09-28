fd853ab73412bb65414df37b9f3a5bcbb2bfd500
aur3l14no <muzhi.yu1@gmail.com>
2024-09-28 21:54:25 +0800

    chore: better `run.ps1`

--- run_example.ps1
+++ run_example.ps1
@@ -1,1 +1,5 @@
-poetry run streamlit run Dashboard.py
+$MyInvocation.MyCommand.Path | Split-Path | Push-Location
+adb kill-server
+adb start-server
+.venv/Scripts/activate.ps1
+streamlit run Dashboard.py
